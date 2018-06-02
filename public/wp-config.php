ed17656c965f6a40bf1bcd8cf789c4b52e027d43
Ammar Faizi <ammarfaizi2@gmail.com>
2018-06-03 02:39:41 +0700

    Update plugins

--- public/wp-config.php
+++ public/wp-config.php
@@ -20,16 +20,18 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
+define('WP_CACHE', true);
+define( 'WPCACHEHOME', '/home/ammarfaizi2/web/blog/public/wp-content/plugins/wp-super-cache/' );
 define('DB_NAME', 'blog');
 
 /** MySQL database username */
 define('DB_USER', 'blog');
 
 /** MySQL database password */
-define('DB_PASSWORD', '858869123');
+define('DB_PASSWORD', 'cnR0IG1pbi9hdmcvbWF4L21kZXYgPSAzO');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '127.0.0.1');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
