5c98ff5ed75ce63581471b14f131438eae908d38
Ammar Faizi <ammarfaizi2@gmail.com>
2018-07-19 00:00:16 +0700

    Daily backup

--- public/wp-content/cache/supercache/69.12.94.61/sitemap.xml/meta-wp-cache-f9746dcca5f602ad4a3430fd6917bfe5.php
+++
@@ -1,1 +0,0 @@
-<?php die(); ?>{"dynamic":true,"uri":"69.12.94.61\/sitemap.xml","blog_id":1,"post":0,"key":"69.12.94.61443\/sitemap.xml","headers":{"Vary":"Vary: Cookie","X-Robots-Tag":"X-Robots-Tag: noindex","Content-Type":"Content-Type: text\/xml; charset=utf-8","Last-Modified":"Last-Modified: Tue, 17 Jul 2018 16:56:34 GMT"}}
