dd2aae27febbab79a3b1345ade3828eb81eeada5
Ammar Faizi <ammarfaizi2@gmail.com>
2018-07-27 00:00:41 +0700

    Daily backup

--- public/wp-content/cache/supercache/webhook.teainside.org/meta-wp-cache-757e555f973a6ee14ca2b8a00528ffa2.php
+++
@@ -1,1 +0,0 @@
-<?php die(); ?>{"dynamic":true,"uri":"webhook.teainside.org\/","blog_id":1,"post":0,"key":"webhook.teainside.org443\/","headers":{"Vary":"Vary: Cookie","Content-Type":"Content-Type: text\/html; charset=UTF-8","Link":"Link: <https:\/\/wp.me\/9XWWk>; rel=shortlink","Last-Modified":"Last-Modified: Wed, 25 Jul 2018 16:32:35 GMT"}}
