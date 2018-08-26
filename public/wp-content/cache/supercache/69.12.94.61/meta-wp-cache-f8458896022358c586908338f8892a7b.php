ca29011c966f65e179c26447170376fdeaacc7a4
Ammar Faizi <ammarfaizi2@gmail.com>
2018-08-27 00:00:13 +0700

    Daily backup

---
+++ public/wp-content/cache/supercache/69.12.94.61/meta-wp-cache-f8458896022358c586908338f8892a7b.php
@@ -0,0 +1,1 @@
+<?php die(); ?>{"dynamic":true,"uri":"69.12.94.61\/","blog_id":1,"post":0,"key":"69.12.94.61443\/","headers":{"Vary":"Vary: Cookie","Content-Type":"Content-Type: text\/html; charset=UTF-8","Link":"Link: <https:\/\/wp.me\/9XWWk>; rel=shortlink","Last-Modified":"Last-Modified: Sun, 26 Aug 2018 16:10:44 GMT"}}
